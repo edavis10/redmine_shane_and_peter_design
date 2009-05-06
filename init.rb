4e7989db4b9de1984efbf123f9256b5d1dbfcef7
Eric Davis <edavis@littlestreamsoftware.com>
2009-05-06 15:05:25 -0700

    Added some project documentation

--- init.rb
+++ init.rb
@@ -2,7 +2,11 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_shane_and_peter_design do
   name 'Redmine Shane And Peter Design plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  author 'Eric Davis'
+  url 'https://redmine.shaneandpeter.com'
+  author_url 'http://www.littlestreamsoftware.com'
+  description "This is a plugin to implement the custom design for Shane and Peter Inc."
+  version '0.1.0'
+
+  requires_redmine :version_or_higher => '0.8.0'
 end
