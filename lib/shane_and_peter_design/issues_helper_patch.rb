8028f53d6d5f0b88989ce7ed143ebb1be0232cfc
Eric Davis <edavis@littlestreamsoftware.com>
2012-03-14 13:55:09 -0700

    [#6898] Remove showing thumbnails for attachments, needs AAJ updates

--- lib/shane_and_peter_design/issues_helper_patch.rb
+++ lib/shane_and_peter_design/issues_helper_patch.rb
@@ -3,7 +3,7 @@ module ShaneAndPeterDesign
     def self.included(base) # :nodoc:
       base.send(:include, InstanceMethods)
       base.class_eval do
-        alias_method_chain :show_detail, :attachment_previews
+        #alias_method_chain :show_detail, :attachment_previews
       end
     end
 
